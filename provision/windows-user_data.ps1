7f11a3a39e38b2e7e5d42e3f8a530a1c1f77e22f
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-06-14 20:52:57 -0400

    Add Windows Server 2016 builder
    Issue: RELENG-939 RELENG-999
    Change-Id: I539afdc08676e3317d823292f0692361ca9c6825
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ provision/windows-user_data.ps1
@@ -0,0 +1,17 @@
+<powershell>
+winrm quickconfig -q
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+
+netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
+netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
+
+net stop winrm
+sc.exe config winrm start= auto
+net start winrm
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
+</powershell>
