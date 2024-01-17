lfit/releng-common-packer

commit 7f5b5f010949c9be63aafdf8a06085739be8a2cf

Chore: Update CentOS Stream 9 to latest
Issue: RELENG-5094
Change-Id: If5d851e1a49f7052e6a4a9e8307109cdf87b4ac7
Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

vars/centos-cs-9.pkrvars.hcl
@@ -1,7 +1,7 @@
 source_ami_filter_name = "*CentOS-cs-9*"
 source_ami_filter_owner = "aws-marketplace"
 source_ami_filter_product_code = "0454011e44daf8e6d"
-base_image = "CentOS Stream 9 (x86_64) [2022-02-24]"
+base_image = "CentOS Stream 9 (x86_64) [2023-03-27]
 distro = "CentOS Stream 9"
 docker_source_image = "centos:9"
 ssh_user = "cloud-user"

commit d4e1f792fb08af9a269710d0bca7c50b53eb5ec8

Fix: Add missing double quote
The 7f5b5f010949c introduced a typo which break the GHA verify.

Issue: RELENG-5094
Change-Id: I767da30bb98b3c4f75d3a94fca76259b0e1a6cc0
Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

vars/centos-cs-9.pkrvars.hcl
@@ -1,7 +1,7 @@
 source_ami_filter_name = "*CentOS-cs-9*"
 source_ami_filter_owner = "aws-marketplace"
 source_ami_filter_product_code = "0454011e44daf8e6d"
-base_image = "CentOS Stream 9 (x86_64) [2023-03-27]
+base_image = "CentOS Stream 9 (x86_64) [2023-03-27]"
 distro = "CentOS Stream 9"
 docker_source_image = "centos:9"
 ssh_user = "cloud-user"